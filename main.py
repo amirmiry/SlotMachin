d104da27b1fc6e72386ddc28e85679b1e0cc6995
amir miri <saturnloop.ai@gmail.com>
2025-11-30 03:36:17 +0330

    add symbol_count and ROWS COLS for new mwthod

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import random
 MAX_LINES = 3
 MAX_BET= 100
 MIN_BET = 1
 
-
-
+ROWS = 3
+COLS = 3
+symbol_count  = {"A": 2,
+                 "B": 4,
+                 "C": 6,
+                 "D": 8,}
 
 def deposite():
     while True:
@@ -71,4 +76,4 @@ def main ():
 
     print(f"your betting {bet} on the {lines} lines, your total bet is ${total_bet}")
 
-main()
+main()  
